7153dc0fc105c074cddcc60ad7838ac3a8f4b374
Aby Babu <abyStriveschool@gmail.com>
2020-06-01 14:42:32 +0200

    Shopping- list and recipe separated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  collapsed = true;
-  constructor() {
-  }
+export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
 
-  ngOnInit(): void {
-  }
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
 
+  }
 }
